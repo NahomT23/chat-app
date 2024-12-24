e0b27d9ac05b0ee79a599fbe6705528c53890ade
Nahom Tsegaye <tnahom214@gmail.com>
2024-12-24 20:39:40 +0300

    fixed for deployment

---
+++ backend/dist/src/routes/message.route.js
@@ -0,0 +1,16 @@
+// import express from 'express'
+// import protectRoute from '../middleware/protectRoute.js'
+// import { sendMessage, getMessages, getUsersForSideBar } from '../controllers/message.controller.js'
+// const router = express.Router()
+// router.get('/conversations', protectRoute, getUsersForSideBar )
+// router.get('/:id', protectRoute, getMessages)
+// router.post('/send/:id', protectRoute, sendMessage)
+// export default router
+import express from "express";
+import protectRoute from "../middleware/protectRoute.js";
+import { getMessages, getUsersForSidebar, sendMessage } from "../controllers/message.controller.js";
+const router = express.Router();
+router.get("/conversations", protectRoute, getUsersForSidebar);
+router.get("/:id", protectRoute, getMessages);
+router.post("/send/:id", protectRoute, sendMessage);
+export default router;
--- backend/src/index.ts
+++ backend/src/index.ts
@@ -4,11 +4,12 @@ import messageRoutes from './routes/message.route.js';
 import dotenv from 'dotenv'
 import { app, server } from './socket/socket.js';
 import cookieParser from "cookie-parser"
+import path from "path"
 
 dotenv.config()
 
 const PORT = process.env.PORT || 5001
-
+const __dirname = path.resolve()
 
 app.use(cookieParser())
 app.use(express.json())
@@ -17,6 +18,12 @@ app.use(express.json())
 app.use('/api/auth', authRoutes);
 app.use('/api/messages', messageRoutes);
 
+if(process.env.Node_ENV !== "development"){
+    app.use(express.static(path.join(__dirname, "/frontend/dist")))
+    app.get("*", (req, res) => {
+        res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"))
+    })
+}
 
 server.listen(PORT, () => {
     console.log(`Server is running on port: ${PORT}`);
